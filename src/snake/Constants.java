7488ba942a209b2dbaba673dc807552e1a309887
Thomas Amstutz <54527950+ThomasAmstutz@users.noreply.github.com>
2020-11-24 13:27:39 +0100

    Création d'un fichier de constantes

---
+++ src/snake/Constants.java
@@ -0,0 +1,53 @@
+package snake;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Constantes du programme
+ */
+public class Constants {
+    /**
+     * Largeur de la fenêtre
+     */
+    public static final int WIDTH = 500;
+
+    /**
+     * Hauteur de la fenêtre
+     */
+    public static final int HEIGHT = 500;
+
+    /**
+     * La taille d'une case de la grille
+     */
+    public static final int TILE_SIZE = 10;
+
+    /**
+     * Couleur de fond de la grille
+     */
+    public static final Color BACKGROUND_COLOR = new Color(0.1, 0.1, 0.1, 1);
+
+    /**
+     * Couleur des textes
+     */
+    public static final Color TEXT_COLOR = Color.BEIGE;
+
+    /**
+     * Chemin vers les images dans l'arborescence de fichiers
+     */
+    public static final String PATH_TO_IMAGES = "/images/";
+
+    /**
+     * Vitesse du jeu
+     */
+    public static double INITIAL_TIMELINE_RATE = 5;
+
+    /**
+     * Longueur du serpent en début de partie
+     */
+    public static final int INITIAL_SNAKE_LENGTH = 2;
+
+    /**
+     * Direction initiale du serpent
+     */
+    public static final Snake.Direction INITIAL_SNAKE_DIRECTION = Snake.Direction.LEFT;
+}
